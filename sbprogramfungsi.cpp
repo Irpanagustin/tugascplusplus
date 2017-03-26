4891546c9e8b154912eda55bc20833f76fe04db1
Irpanagustin <irvanagustin57@gmail.com>
2017-03-26 12:22:01 +0700

    latihan modular

---
+++ sbprogramfungsi.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+// {i.s} Inisialisasi Fungsi program
+// {f.s} Pemanggilan Fungsi program
+using namespace std;
+
+// Inisialisasi Fungsi
+ void contoh() {
+  cout << "Ini Hanya latihan " << endl;
+
+}
+
+//Inisialisai Fungsi
+ void Nama(string nama) {
+  cout << "Haiii" << nama << endl;
+}
+
+int main(){
+
+  contoh(); // pemanggilan fungsi
+
+
+  Nama("Irpan Agustin"); // pemanggilan fungsi
+
+  cout << "Apa Kabar" << endl;
+cout << "Lagi Ngapain" << endl;
+  return 0;
+}
